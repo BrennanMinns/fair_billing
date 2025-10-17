e16b83cc3df174233ad4c0fd3c1c427ff7e9633f
Brennan Minns <brennan.minns@bt.com>
2025-10-17 03:30:00 +0100

    update readme with instructions

---
+++ README.rb
@@ -0,0 +1,9 @@
+# =-=-= Setup =-=-=
+
+# in order to make it executable, you may need to run:
+  # chmod +x bin/fair_billing.rb
+
+# Be sure to run 'bundle install' first to install rspec 
+
+# To run the program:
+  # bin/fair_billing.rb examples/sample_log.txt 
