36882d8f92b6ce54fbce6b7098f387bfa9fbaa0f
Brennan Minns <brennan.minns@bt.com>
2025-10-17 03:24:15 +0100

    add initial logic for log_parser

--- lib/fair_billing/log_parser.rb
+++ lib/fair_billing/log_parser.rb
@@ -6,6 +6,14 @@ module FairBilling
 
     def parse
       lines = File.readlines(@file_path).map(&:strip)
+      entries = lines.map { |line| LogEntry.new(line) }.select(&:valid?)
+      if entries.empty?
+        [[], nil, nil]
+      else
+        min_time = entries.map(&:time_seconds).min
+        max_time = entries.map(&:time_seconds).max
+        [entries, min_time, max_time]
+      end
     end
   end
 end
