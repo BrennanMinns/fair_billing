06070115f1768599b5d48cc226264524a7bac7d2
Brennan Minns <brennan.minns@bt.com>
2025-10-17 03:24:52 +0100

    add initial logic for log_parser

--- lib/fair_billing/log_parser.rb
+++ lib/fair_billing/log_parser.rb
@@ -10,9 +10,6 @@ module FairBilling
       if entries.empty?
         [[], nil, nil]
       else
-        min_time = entries.map(&:time_seconds).min
-        max_time = entries.map(&:time_seconds).max
-        [entries, min_time, max_time]
       end
     end
   end
