81f98bcddc80de928a0a984729e662e2d2987c02
Brennan Minns <brennan.minns@bt.com>
2025-10-17 03:27:42 +0100

    utilise log_parser inside fair_billing class

--- lib/fair_billing.rb
+++ lib/fair_billing.rb
@@ -5,8 +5,14 @@ require 'fair_billing/user_sessions'
 module FairBilling
   def self.run(file_path)
     parser = LogParser.new(file_path)
+    entries, min_time, max_time = parser.parse
 
     calculator = UserSessions.new(entries, min_time, max_time)
+    results = calculator.compute_results
+
+    results.sort_by { |user, _| user }.each do |user, (sessions, duration)|
+      puts "#{user} #{sessions} #{duration}"
+    end
 
   rescue StandardError => e
     warn "Error: #{e.message}"
