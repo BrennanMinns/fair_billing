1a3e19f99fda46ba34b1da1be50aede7dd7dd7a4
Brennan Minns <brennan.minns@bt.com>
2025-10-17 03:25:37 +0100

    add tests for log_entry

--- spec/log_entry_spec.rb
+++ spec/log_entry_spec.rb
@@ -2,5 +2,32 @@ require 'fair_billing'
 
 describe FairBilling::LogEntry do 
   describe '#initialize' do
+    it 'parses valid line' do
+      entry = FairBilling::LogEntry.new('14:02:03 ALICE99 Start')
+      expect(entry.time_seconds).to eq(14 * 3600 + 2 * 60 + 3)
+      expect(entry.user).to eq('ALICE99')
+      expect(entry.action).to eq('Start')
+      expect(entry.valid?).to be true
+    end
+
+    it 'ignores invalid line' do
+      entry = FairBilling::LogEntry.new('invalid')
+      expect(entry.valid?).to be false
+    end
+
+    it 'ignores invalid time' do
+      entry = FairBilling::LogEntry.new('25:00:00 USER Start')
+      expect(entry.valid?).to be false
+    end
+
+    it 'ignores invalid user' do
+      entry = FairBilling::LogEntry.new('25:00:00 1 Start')
+      expect(entry.valid?).to be false
+    end
+
+    it 'ignores invalid start/end' do
+      entry = FairBilling::LogEntry.new('25:00:00 1 invalid')
+      expect(entry.valid?).to be false
+    end
   end
 end
